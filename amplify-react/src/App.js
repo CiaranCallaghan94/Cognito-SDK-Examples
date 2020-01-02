70003a0d6fd7469f23d68ce2e41e7d4399774f67
Ciaran Callaghan <ciarancallaghan94@gmail.com>
2020-01-02 16:19:57 +0000

    Add Hosted UI and Sign in functionalitlity without HOC

--- amplify-react/src/App.js
+++ amplify-react/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Amplify from 'aws-amplify';
 import { withAuthenticator } from 'aws-amplify-react'; // or 'aws-amplify-react-native';
-import { Auth } from 'aws-amplify';
+import { Auth, Hub} from 'aws-amplify';
 
 Amplify.configure({
   Auth: {
@@ -16,10 +16,40 @@ Amplify.configure({
       userPoolWebClientId: '1rtmin31d9unkfpteemlj3ffdm',
       // - Enforce user authentication prior to accessing AWS resources or not
       mandatorySignIn: false,
+
+	// OPTIONAL - Hosted UI configuration
+	  oauth: {
+		domain: 'cog-sdk-examples.auth.eu-west-1.amazoncognito.com',
+		scope: ['phone', 'email', 'profile', 'openid', 'aws.cognito.signin.user.admin'],
+		redirectSignIn: 'http://localhost:3000/',
+		redirectSignOut: 'http://localhost:3000/',
+		responseType: 'token' // or 'token', note that REFRESH token will only be generated when the responseType is code
+	  }
   }
 });
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    Hub.listen('auth', (data) => {
+		console.log(data);
+        switch (data.payload.event) {
+            case 'signIn':
+                alert("Signed in");
+                break;
+            case 'signIn_failure':
+                alert("Sign in failure");
+                break;
+			case  'signOut':
+				alert("Sign Out");
+                break;
+            default:
+                break;
+        }
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -28,15 +58,19 @@ class App extends React.Component {
             Below are some examples of Cognito functionality written using Amplify SDK
           </p>
         </header>
+		<SignIn/>
+		<OauthSignIn/>
         <SignOut/>
         <CurrentAuthenticatedUser/>
         <RetrieveCurrentSession/>
+
       </div>
     );
   }
 }
 
-export default withAuthenticator(App, false);
+//export default withAuthenticator(App, false);
+export default App
 
 //  Functionality Components
 // Global Sign Out
@@ -107,3 +141,121 @@ class RetrieveCurrentSession extends React.Component {
     );
   }
 }
+
+class OauthSignIn extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleFederatedSignIn = this.handleFederatedSignIn.bind(this);
+  }
+  handleFederatedSignIn(){
+    Auth.federatedSignIn()
+  }
+
+  render() {
+    return (
+    <div className="Amplify-component">
+      <h4>HostedUI Sign In</h4>
+      <button onClick={this.handleFederatedSignIn}>Go to</button>
+    </div>
+    );
+  }
+}
+
+class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSignIn = this.handleSignIn.bind(this);
+  }
+  async handleSignIn(){
+     try {
+		// Test Data
+		var username = "testUser"
+		var password = "Password123!"
+        const user = await Auth.signIn(username, password);
+        if (user.challengeName === 'SMS_MFA' ||
+            user.challengeName === 'SOFTWARE_TOKEN_MFA') {
+            // You need to get the code from the UI inputs
+            // and then trigger the following function with a button click
+           // const code = getCodeFromUserInput();
+           // If MFA is enabled, sign-in should be confirmed with the confirmation code
+           // const loggedUser = await Auth.confirmSignIn(
+           //    user,   // Return object from Auth.signIn()
+           //     code,   // Confirmation code  
+           //     mfaType // MFA Type e.g. SMS_MFA, SOFTWARE_TOKEN_MFA
+           // );
+        } else if (user.challengeName === 'NEW_PASSWORD_REQUIRED') {
+            // const {requiredAttributes} = user.challengeParam; // the array of required attributes, e.g ['email', 'phone_number']
+            // You need to get the new password and required attributes from the UI inputs
+            // and then trigger the following function with a button click
+            // For example, the email and phone_number are required attributes
+            // const {username, email, phone_number} = getInfoFromUserInput();
+            // const loggedUser = await Auth.completeNewPassword(
+            //    user,              // the Cognito User Object
+            //    newPassword,       // the new password
+                // OPTIONAL, the required attributes
+            //    {
+            //        email,
+            //        phone_number,
+            //    }
+            //);
+        } else if (user.challengeName === 'MFA_SETUP') {
+            // This happens when the MFA method is TOTP
+            // The user needs to setup the TOTP before using it
+            // More info please check the Enabling MFA part
+            Auth.setupTOTP(user);
+        } else {
+            // The user directly signs in
+            console.log(user);
+        }
+    } catch (err) {
+        if (err.code === 'UserNotConfirmedException') {
+            // The error happens if the user didn't finish the confirmation step when signing up
+            // In this case you need to resend the code and confirm the user
+            // About how to resend the code and confirm the user, please check the signUp part
+        } else if (err.code === 'PasswordResetRequiredException') {
+            // The error happens when the password is reset in the Cognito console
+            // In this case you need to call forgotPassword to reset the password
+            // Please check the Forgot Password part.
+        } else if (err.code === 'NotAuthorizedException') {
+            // The error happens when the incorrect password is provided
+        } else if (err.code === 'UserNotFoundException') {
+            // The error happens when the supplied username/email does not exist in the Cognito user pool
+        } else {
+            console.log(err);
+        }
+    }
+  }
+
+  render() {
+    return (
+    <div className="Amplify-component">
+      <h4>Sign In as testUser</h4>
+      <button onClick={this.handleSignIn}>Sign In</button>
+    </div>
+    );
+  }
+}
+
+// New Component
+/*
+class RetrieveCurrentSession extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleRetrieveCurrentSession = this.handleRetrieveCurrentSession.bind(this);
+  }
+  handleRetrieveCurrentSession(){
+    Auth.currentSession()
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+    <div className="Amplify-component">
+      <h4>Retrieve Current Session</h4>
+      <button onClick={this.handleRetrieveCurrentSession}>Check Console</button>
+    </div>
+    );
+  }
+}
+*/
